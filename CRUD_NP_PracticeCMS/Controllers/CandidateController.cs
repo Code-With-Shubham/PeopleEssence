fd6fff4d80df47ed1fb1ae30e67fb04c1def6e1c
shubhammanohare88@gmail.com <shubhammanohare88@gamil.com>
2024-08-16 13:23:12 +0530

    Commit By Shubham

--- CRUD_NP_PracticeCMS/Controllers/CandidateController.cs
+++ CRUD_NP_PracticeCMS/Controllers/CandidateController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLogicLayer.Models;
+using DataContext.Data;
+using DataContext.TableEntities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CRUD_NP_PracticeCMS.Controllers
 {
@@ -7,6 +10,85 @@ namespace CRUD_NP_PracticeCMS.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [Route("Candidate/Submit")]
+        public async Task<IActionResult> Submit([FromBody] CandidateVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (ApplicationDbContext db = new ApplicationDbContext())
+                    {
+                        Candidate candidate = new Candidate
+                        {
+                            CandidateId = model.CandidateId,
+                            FirstName = model.FirstName,
+                            LastName = model.LastName,
+                            Email = model.Email,
+                            PhoneNumber = model.PhoneNumber,
+                            Gender = model.Gender,
+                            Address = model.Address,
+                            Resumefile = model.Resumefile,
+                            AppliedPosition = model.AppliedPosition,
+                            Skills = model.Skills,
+                            CurrentLocation = model.CurrentLocation,
+                            PreferedLocation = model.PreferedLocation,
+                            CurrentCtc = model.CurrentCtc,
+                            ExpectedCtc = model.ExpectedCtc,
+                            References = model.References,
+                            ReferencesEmpId = model.ReferencesEmpId,
+                            LinkedInLink = model.LinkedInLink,
+                            RegistrationDate = DateOnly.FromDateTime(DateTime.Now), 
+
+                        };
+                        db.Candidates.Add(candidate);
+                        int cnt = await db.SaveChangesAsync();
+
+                        if (cnt > 0)
+                        {
+                            CandidatesDetail candidatesDetail = new CandidatesDetail
+                            {
+                                PreviousEmp = model.PreviousEmp,
+                                FromDate = DateOnly.FromDateTime((DateTime)model.FromDate),
+                                ToDate = DateOnly.FromDateTime((DateTime)model.ToDate),
+                                Education = model.Education,
+                                EduFromDate = DateOnly.FromDateTime((DateTime)model.EduFromDate),
+                                EduToDate = DateOnly.FromDateTime((DateTime)model.EduToDate)
+                            };
+
+                            db.Candidates.Add(candidate);
+                            int cnt1 = await db.SaveChangesAsync();
+                            if (cnt > 0)
+                            {
+                                return Json(new { success = true, message = "Candidate information submitted successfully!" });
+                            }
+                            else
+                            {
+                                return Json(new { success = false, message = "Candidate information submission failed!" });
+                            }
+
+                        }
+                        else
+                        {
+                            return Json(new { success = false, message = "Candidate information submission failed!" });
+                        }
+
+                    }
+
+                    
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { success = false, message = $"An error occurred: {ex.Message}" });
+                }
+            }
+            else
+            {
+                return Json(new { success = false, message = "Validation failed.", errors = ModelState.Values });
+            }
         }
     }
 }
